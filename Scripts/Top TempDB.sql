e39fa27161ad5deeeb095292be166cefcb9bd50e
Matheus Nunes Rossi <126213415+DBAmatheusNunesRossi@users.noreply.github.com>
2026-05-05 13:14:45 -0300

    FTop TempDB.sql
    Ele:
    ✅ Mostra sessões que estão consumindo espaço
    ✅ Identifica usuário e máquina
    ✅ Mostra quanto cada sessão alocou (em MB)
    ✅ Exibe a query associada
    ✅ Soma o total consumido

---
+++ Scripts/Top TempDB.sql
@@ -0,0 +1,20 @@
+;WITH tab(session_id, host_name, login_name, TotalAlocadoMB, text) AS
+  (SELECT a.session_id,
+          b.host_name,
+          b.login_name,
+          (user_objects_alloc_page_count + internal_objects_alloc_page_count) * 1.0 / 128 AS TotalAlocadoMB,
+          d.TEXT
+   FROM sys.dm_db_session_space_usage a
+   JOIN sys.dm_exec_sessions b ON a.session_id = b.session_id
+   JOIN sys.dm_exec_connections c ON c.session_id = b.session_id CROSS APPLY sys.Dm_exec_sql_text(c.most_recent_sql_handle) AS d
+   WHERE a.session_id > 50)
+SELECT *
+FROM tab
+UNION ALL
+SELECT NULL,
+       NULL,
+       'TOTAL ALOCADO',
+       sum(totalalocadomb),
+       NULL
+FROM tab
+ORDER BY 4 DESC
